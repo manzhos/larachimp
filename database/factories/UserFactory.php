68736977141731d9f9d4bcd0a3628a91a2e2c73e
Vladimir Manzhos <manzhos@gmail.com>
2021-02-05 02:55:01 +0200

    correct seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,9 +27,8 @@ class UserFactory extends Factory
             'name' => $this->faker->firstName,
             'surname' => $this->faker->lastName,
             'email' => preg_replace('/@example\..*/', '@gmail.com', $this->faker->unique()->safeEmail),
-            //'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            //'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            //'remember_token' => Str::random(10),
             'subscribe_status' => 0,
             'syncdate' => null
         ];
